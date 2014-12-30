964c72f75bd51be669eb67c549f3df8d82a9638b
Jack Wang <wangzhe.cs@gmail.com>
2014-12-30 17:24:47 +0800

    Jack: googleapi python works

---
+++ test/__init__.py
@@ -0,0 +1,9 @@
+#import os
+
+__title__ = 'jira-service'
+__version__ = '0.1'
+__description__ = 'Python client for JIRA SOAP API / Google Calendar API'
+__url__ = 'https://github.com/wangzhe/jira-service'
+__author__ = 'Jack Wang'
+__licence__ = 'MIT'
+__copyright__ = 'Copyright 2014 Jack\'s House'
