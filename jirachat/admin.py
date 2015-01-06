c9d0904a605c79d8e5090ada4724f0099a046ded
Jack Wang <wangzhe.cs@gmail.com>
2015-01-06 18:54:30 +0800

    Jack: add djangle service in wechat

--- jirachat/admin.py
+++ jirachat/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Line
+
 
 # Register your models here.
+admin.site.register(Line)
--- wechat_server/urls.py
+++ wechat_server/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from jirachat.views import home
+from jirachat import views
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'jirachat.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
     # url(r'^jirachat/', include(jirachat.site.urls)),
+
+    url(r'^$', 'jirachat.views.home', name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^wechat/', 'jirachat.views.wechat', name='wechat'),
 )
